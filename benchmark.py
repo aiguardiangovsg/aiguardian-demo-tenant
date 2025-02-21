54caabfc9cd08f44fbbaabe7819159cc10c916be
Quy Tang <3761730+qtangs@users.noreply.github.com>
2025-02-21 09:32:12 +0800

    Add timeout in Litmus test run execution

--- benchmark.py
+++ benchmark.py
@@ -179,6 +179,7 @@ def main():
         "endpoint": endpoint,
         "test_suites": [input_value],
         "num_of_prompts": num_of_prompts,
+        "timeout_in_seconds": timeout,
     }
 
     log(
